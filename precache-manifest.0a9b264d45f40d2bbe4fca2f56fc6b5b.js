8183c28dde4746d2e992dc4dcf474acd5656fd90
AntonChetveryk <chetverykanton92@gmail.com>
2020-04-07 16:12:05 +0300

    Updates

---
+++ precache-manifest.0a9b264d45f40d2bbe4fca2f56fc6b5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd029ecb4f8eeed941c7c32726022936",
+    "url": "/cursor-router/index.html"
+  },
+  {
+    "revision": "c9ad8f6ea28467e2ffbd",
+    "url": "/cursor-router/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a2ac42a9f67b4f80efc7",
+    "url": "/cursor-router/static/css/main.b078d438.chunk.css"
+  },
+  {
+    "revision": "c9ad8f6ea28467e2ffbd",
+    "url": "/cursor-router/static/js/2.1acfc91c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cursor-router/static/js/2.1acfc91c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2ac42a9f67b4f80efc7",
+    "url": "/cursor-router/static/js/main.96be05bf.chunk.js"
+  },
+  {
+    "revision": "7769d7f62f296fad75a3",
+    "url": "/cursor-router/static/js/runtime-main.838a3448.js"
+  }
+]);
